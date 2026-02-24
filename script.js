35b31834ed906812830f131cf60a3cc68fe00c7e
sachin singh <sachinsingh@sachins-MacBook-Air.local>
2026-02-24 23:50:19 +0530

    DONE by sachin singh

--- script.js
+++ script.js
@@ -28,6 +28,19 @@ function getBotResponse(input) {
     if (input.includes("name")) return "I am your AI Assistant.";
     if (input.includes("how are you")) return "I am just code, but I'm doing great!";
     if (input.includes("bye")) return "Goodbye! Have a nice day!";
+    if (input.includes("who created you")) return "I was modified by an MCA student!";
+    if (input.includes("radhe radhe")) return "RADHE RADHE";
+    if (input.includes("time")) return "I Don't know !";
     
     return "Sorry, I don't understand that yet.";
 }
+document.getElementById("clear-btn").addEventListener("click", () => {
+    const chatBox = document.getElementById("chat-box");
+    // This clears the messages and puts the original greeting back
+    chatBox.innerHTML = '<div class="bot-message">Hello! How can I help you today?</div>';
+});
+
+
+document.getElementById("darkModeBtn").addEventListener("click",() => {
+    document.body.classList.toggle("dark-mode");
+});
